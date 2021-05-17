d2e8a80e8313dc3abdb14a8bb7f9f96f9922bbb3
fanny.grennerat <fanny.grennerat@gmail.com>
2021-05-16 21:47:00 -0600

    Calculation of products of the buttons that you choose.

--- src/main/java/fr/fgrennerat/calculusgame/Main.java
+++ src/main/java/fr/fgrennerat/calculusgame/Main.java
@@ -1,14 +1,23 @@
 package fr.fgrennerat.calculusgame;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main extends Application {
 
@@ -16,18 +25,31 @@ public class Main extends Application {
     public void start(Stage stage) {
 
         TilePane tilePane = new TilePane();
-        Scene scene = new Scene(tilePane, 600, 450);
+        Scene scene = new Scene(tilePane, 1600, 450);
         stage.setTitle("Wonder Game");
         stage.setScene(scene);
 
+        //Creation de boutons
         ArrayList<Button> listeBoutons = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
+        final int[] product = {1};
+        for (int i = 1; i < 30; i++) {
             Button button = new Button(String.valueOf(i));
-            button.setPrefSize(200, 150);
+            button.setPrefSize(40, 20);
             listeBoutons.add(button);
+            // action event
+            final int finalI = i;
+            EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent e){
+                    product[0] *= finalI;
+                    button.setText(String.valueOf(product[0]));
+                    button.setTextFill(Color.INDIANRED);
+                    //button.setStyle();
+                }
+            };
+
+            // when button is pressed
+            button.setOnAction(event);
         }
-
-
         //Creating the tile pane
 
         //Setting the orientation for the Tile Pane
@@ -35,90 +57,24 @@ public class Main extends Application {
         //Setting the alignment for the Tile Pane
         tilePane.setTileAlignment(Pos.CENTER_LEFT);
         //Setting the preferred columns for the Tile Pane
-        tilePane.setPrefRows(3);
+        tilePane.setPrefRows(10);
         //Retrieving the observable list of the Tile Pane
         tilePane.getChildren().addAll(listeBoutons);
 
+        //Creation d'un cadre texte
+        Text zoneTextuelle = new Text("Click on numbers to obtain the product 45 ");
+        //zoneTextuelle.setWrappingWidth(400);
+        tilePane.getChildren().add(zoneTextuelle);
+        //zoneTextuelle.getChildren().add("asdfadsf")
+
+        //Setting the stage
+        //Scene scene = new Scene(root, 595, 200, Color.BEIGE);
+        
+        
+        ///////
         stage.show();
     }
     public static void main(String args[]){
         launch(args);
     }
 }
-/*
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-
-public class Main extends Application {
-    public static void main(String[] args) {
-        System.out.println("Starting app...");
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Calculus Wonder Game");
-
-        // create a button
-        Button b = new Button("button");
-
-        // create a stack pane
-        TilePane r = new TilePane();
-
-        // create a label
-        Label l = new Label("button not selected");
-
-        // action event
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                l.setText("   button   selected    ");
-            }
-        };
-
-        // when button is pressed
-        b.setOnAction(event);
-
-        // add button
-        r.getChildren().add(b);
-        r.getChildren().add(l);
-
-        primaryStage.show();
-    }
-}*/
-
-    // Java Program to create a button and add it to the stage
-
-    /*public class button extends Application {
-
-        // launch the application
-        public void start(Stage s)
-        {
-            // set title for the stage
-            s.setTitle("creating buttons");
-
-            // create a button
-            Button b = new Button("button");
-
-            // create a stack pane
-            StackPane r = new StackPane();
-
-            // add button
-            r.getChildren().add(b);
-
-            // create a scene
-            Scene sc = new Scene(r, 200, 200);
-
-            // set the scene
-            s.setScene(sc);
-
-            s.show();
-        }
-    }
-
-}
-
-*/
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ open module fr.fgrennerat.calculusgame {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.swing;
+    requires javafx.graphics;
 }
