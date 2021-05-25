9ce8e32c300437f44cd751b1ce7aa4a6fab202ed
fanny.grennerat <fanny.grennerat@gmail.com>
2021-05-24 18:09:40 -0600

    First version with response and random number to find It has changed into a minesweeper for a moment (thank you Almas)

--- src/main/java/fr/fgrennerat/calculusgame/Main.java
+++ src/main/java/fr/fgrennerat/calculusgame/Main.java
@@ -6,9 +6,18 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
@@ -17,76 +26,140 @@ import javafx.stage.Stage;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main extends Application {
+    private static final int TILE_SIZE = 40;
+    private static final int W = 800;
+    private static final int H = 600;
 
+    private static final int X_TILES = W / TILE_SIZE;
+    private static final int Y_TILES = H / TILE_SIZE;
 
+    private Tile[][] grid = new Tile[X_TILES][Y_TILES];
+    private Scene scene;
+
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(W, H);
+
+        for (int y = 0; y < Y_TILES; y++) {
+            for (int x = 0; x < X_TILES; x++) {
+                Tile tile = new Tile(x, y, Math.random() < 0.2);
+
+                grid[x][y] = tile;
+                root.getChildren().add(tile);
+            }
+        }
+
+        for (int y = 0; y < Y_TILES; y++) {
+            for (int x = 0; x < X_TILES; x++) {
+                Tile tile = grid[x][y];
+
+                if (tile.hasBomb)
+                    continue;
+
+                long bombs = getNeighbors(tile).stream().filter(t -> t.hasBomb).count();
+
+                if (bombs > 0)
+                    tile.text.setText(String.valueOf(bombs));
+            }
+        }
+
+        return root;
+    }
+
+    private List<Tile> getNeighbors(Tile tile) {
+        List<Tile> neighbors = new ArrayList<>();
+
+        // ttt
+        // tXt
+        // ttt
+
+        int[] points = new int[] {
+                -1, -1,
+                -1, 0,
+                -1, 1,
+                0, -1,
+                0, 1,
+                1, -1,
+                1, 0,
+                1, 1
+        };
+
+        for (int i = 0; i < points.length; i++) {
+            int dx = points[i];
+            int dy = points[++i];
+
+            int newX = tile.x + dx;
+            int newY = tile.y + dy;
+
+            if (newX >= 0 && newX < X_TILES
+                    && newY >= 0 && newY < Y_TILES) {
+                neighbors.add(grid[newX][newY]);
+            }
+        }
+
+        return neighbors;
+    }
+
+    private class Tile extends StackPane {
+        private int x, y;
+        private boolean hasBomb;
+        private boolean isOpen = false;
+
+        private Rectangle border = new Rectangle(TILE_SIZE - 2, TILE_SIZE - 2);
+        private Text text = new Text();
+
+        public Tile(int x, int y, boolean hasBomb) {
+            this.x = x;
+            this.y = y;
+            this.hasBomb = hasBomb;
+
+            border.setStroke(Color.LIGHTGRAY);
+
+            text.setFont(Font.font(18));
+            text.setText(hasBomb ? "X" : "");
+            text.setVisible(false);
+
+            getChildren().addAll(border, text);
+
+            setTranslateX(x * TILE_SIZE);
+            setTranslateY(y * TILE_SIZE);
+
+            setOnMouseClicked(e -> open());
+        }
+
+        public void open() {
+            if (isOpen)
+                return;
+
+            if (hasBomb) {
+                System.out.println("Game Over");
+                scene.setRoot(createContent());
+                return;
+            }
+
+            isOpen = true;
+            text.setVisible(true);
+            border.setFill(null);
+
+            if (text.getText().isEmpty()) {
+                getNeighbors(this).forEach(Tile::open);
+            }
+        }
+    }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(createContent());
 
-        TilePane tilePane = new TilePane();
-        Scene scene = new Scene(tilePane, 1600, 450);
-        stage.setTitle("Wonder Game");
         stage.setScene(scene);
-        // génération d'un entier >= 0 et < ***
-        int nAlea = (int) (Math.random() * 500);
-        //Creation de boutons
-        ArrayList<Button> listeBoutons = new ArrayList<>();
-        final int[] product = {1};
-        for (int i = 2; i < 91; i++) {
-            Button button = new Button(String.valueOf(i));
-            button.setPrefSize(100, 20);
-            listeBoutons.add(button);
-            // action event
-            final int finalI = i;
-            EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent e){
-                    product[0] *= finalI;
-                    button.setText(String.valueOf(product[0]));
-                    button.setTextFill(Color.INDIANRED);
-                    if (product[0] == nAlea) {
-                        Text zoneReponse = new Text("Bravo !! ");
-                        tilePane.getChildren().add(zoneReponse);
-                    }
-                    if (product[0] > nAlea) {
-                        Text zoneReponse = new Text("Non :-( ");
-                        tilePane.getChildren().add(zoneReponse);
-                    }
-                    //button.setStyle();
-                }
-            };
-
-            // when button is pressed
-            button.setOnAction(event);
-        }
-        //Creating the tile pane
-
-        //Setting the orientation for the Tile Pane
-        tilePane.setOrientation(Orientation.HORIZONTAL);
-        //Setting the alignment for the Tile Pane
-        tilePane.setTileAlignment(Pos.CENTER_LEFT);
-        //Setting the preferred columns for the Tile Pane
-        tilePane.setPrefRows(10);
-        //Retrieving the observable list of the Tile Pane
-        tilePane.getChildren().addAll(listeBoutons);
-
-
-        //Creation d'un cadre texte
-        Text zoneTextuelle = new Text(new StringBuilder().append("Click on numbers to obtain the product ").append(nAlea).toString());
-        //zoneTextuelle.setWrappingWidth(400);
-        tilePane.getChildren().add(zoneTextuelle);
-        //zoneTextuelle.getChildren().add("asdfadsf")
-
-        //Setting the stage
-        //Scene scene = new Scene(root, 595, 200, Color.BEIGE);
-
-        
-        ///////
         stage.show();
     }
-    public static void main(String args[]){
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
